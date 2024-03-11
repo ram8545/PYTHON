41911ce91c388e1cba8f27451da45abcf1504338
Ram Sevak Mishra <ramsevakmishra69@gmail.com>
2024-03-11 15:22:00 +0530

    caesar clipher

---
+++ Problem/caesarCipher.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'caesarCipher' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER k
+#
+
+def caesarCipher(s, k):
+    # Write your code here
+    encrypted = ''
+    for char in s:
+        if char.isalpha():
+            base = ord('a') if char.islower() else ord('A')
+            encrypted += chr((ord(char) - base + k) % 26 + base)
+        else:
+            encrypted += char
+    return encrypted
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
