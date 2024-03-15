9c977666be0775a9b25f9be6e20c15b9b7111803
Ram Sevak Mishra <ramsevakmishra69@gmail.com>
2024-03-15 13:07:41 +0530

    pairs

---
+++ Problem/pairs.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'pairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def pairs(k, arr):
+    # Write your code here
+    num_set = set(arr)
+    count = 0
+
+    # Iterate through the array elements
+    for num in arr:
+        # Check if num + k exists in the set
+        if num + k in num_set:
+            count += 1
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
