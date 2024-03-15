7f7fa1b16e4dcd929e25e13c79d4ea16843686a7
Ram Sevak Mishra <ramsevakmishra69@gmail.com>
2024-03-15 13:05:34 +0530

    Is balanced

---
+++ Problem/isBalanced.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'isBalanced' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def isBalanced(s):
+    # Write your code here
+    stack = []
+    pair = {'(': ')', '{': '}', '[': ']'}
+    for i in s:
+        if i in pair:
+            stack.append(i)
+        else:
+            if stack and pair[stack[-1]] == i:
+                stack.pop(-1)
+            else:
+                return "NO"
+    return "YES" if not stack else "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        s = input()
+
+        result = isBalanced(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
